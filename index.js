52bf6355891c76c8f73999e69e534909a76eff66
Jonas Huckestein <jonas.huckestein@gmail.com>
2016-02-04 16:25:49 +0000

    CORS headers for OPTIONS

--- index.js
+++ index.js
@@ -9,9 +9,14 @@ app.set("json spaces", 4);
 var jwtSecret = "mondo is the best";
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  if(req.method == "OPTIONS") {
+    res.send(200);
+  } else {
+    next();
+  }
 });
 
 app.get("/robots.txt", function(req, res) {
